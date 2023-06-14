521b88591c52a9cec6906587c01e98a0584f4434
Ben <greatmaharg@gmail.com>
2023-06-14 15:30:42 -0400

    Added vector math library from ArdIU

---
+++ include/VectorMath.h
@@ -0,0 +1,61 @@
+#ifndef VectorMath_h
+#define VectorMath_h
+/**
+VectorMath library for ArdIU
+*/
+
+#include <math.h>
+//#include "Arduino.h"
+
+enum StockVector {
+	V_ZEROES,
+	V_ONES,
+	V_X,
+	V_Y,
+	V_Z
+};
+
+class QuatF; // pre-declare so we can use it in VectorF
+
+// using the Printable interface here burns an additional 2 bytes to point to a VFtable;
+// whether that is worth the convenience is an open quesiton; looking into better ways
+class VectorF/*: public Printable*/ {
+  public:
+	float x, y, z;
+	VectorF(float xval, float yval, float zval);
+	VectorF(StockVector v);
+	VectorF(QuatF q);
+	VectorF();
+
+//	size_t printTo(Print&) const;
+
+	float magSq() const;
+	float mag() const;
+	VectorF negate() const;
+	inline VectorF operator - () const { return negate(); }
+	VectorF add(VectorF other) const;
+	inline VectorF operator + (VectorF other) const { return add(other); }
+	VectorF scale(float s) const;
+	inline VectorF operator * (float s) const { return scale(s); }
+	inline VectorF normalize() const { return scale(1/mag()); }
+	float dot(const VectorF &other) const;
+	VectorF cross(const VectorF &other) const;
+	VectorF rotate(const QuatF &q) const;
+};
+
+class QuatF/*: public Printable*/ {
+  public:
+	float w, x, y, z;
+	QuatF(float wval, float xval, float yval, float zval);
+	QuatF(const VectorF& lv);
+	QuatF();
+	
+//	size_t printTo(Print& to) const;
+	
+	QuatF conj() const;
+	inline QuatF operator ~ () const { return conj(); }
+	QuatF mult(const QuatF& other) const;
+	inline QuatF operator * (const QuatF& other) const { return mult(other); }
+};
+
+#endif
---
+++ src/VectorMath.cpp
@@ -0,0 +1,101 @@
+#include "VectorMath.h"
+
+VectorF::VectorF( float xval,  float yval,  float zval): VectorF() {
+    x = xval;
+    y = yval;
+    z = zval;
+}
+VectorF::VectorF(StockVector v): VectorF() {
+	switch(v) {
+		case V_ZEROES:
+			x = 0; y = 0; z = 0;
+			break;
+		case V_ONES:
+			x = 1; y = 1; z = 1;
+			break;
+		case V_X:
+			x = 1; y = 0; z = 0;
+			break;
+		case V_Y:
+			x = 0; y = 1; z = 0;
+			break;
+		case V_Z:
+			x = 0; y = 0; z = 1;
+			break;
+	}
+}
+VectorF::VectorF(QuatF q): VectorF(q.x, q.y, q.z) {}
+VectorF::VectorF() {}
+
+// size_t VectorF::printTo(Print& to) const {
+	// return to.print("[")+to.print(x)+to.print(" ")
+						// +to.print(y)+to.print(" ")
+						// +to.print(z)+to.print("]");
+// }
+
+float VectorF::magSq() const {
+	return x*x + y*y + z*z;
+}
+float VectorF::mag() const {
+	return sqrt(magSq());
+}
+VectorF VectorF::negate() const {
+	return VectorF(-x, -y, -z);
+}
+VectorF VectorF::add(VectorF other) const {
+	return VectorF(x+other.x, y+other.y, z+other.z);
+}
+VectorF VectorF::scale(float s) const {
+	return VectorF(s*x, s*y, s*z);
+}
+float VectorF::dot(const VectorF &other) const {
+    return x*other.x + y*other.y + z*other.z;
+}
+
+VectorF VectorF::cross(const VectorF &other) const {
+    VectorF newV;
+	newV.x = y*other.z - z*other.y;
+	newV.y = z*other.x - x*other.z;
+	newV.z = x*other.y - y*other.x;
+	return newV;
+}
+
+VectorF VectorF::rotate(const QuatF& q) const {
+	return VectorF(q * QuatF(*this) * ~q);
+}
+
+/**
+ QuatF functions
+ */
+
+QuatF::QuatF(float wval, float xval, float yval, float zval) {
+	w = wval;
+	x = xval;
+	y = yval;
+	z = zval;
+}
+QuatF::QuatF(const VectorF& lv) {
+	w = 0;
+	x = lv.x;
+	y = lv.y;
+	z = lv.z;
+}
+QuatF::QuatF() {}
+
+// size_t QuatF::printTo(Print& to) const {
+	// return to.print("[")+to.print(w)+to.print(" ")
+						// +to.print(x)+to.print(" ")
+						// +to.print(y)+to.print(" ")
+						// +to.print(z)+to.print("]");
+// }
+QuatF QuatF::conj() const {
+	return QuatF(w, -x, -y, -z);
+}
+QuatF QuatF::mult(const QuatF& other) const {
+	QuatF newQ;
+	newQ.w = w*other.w - x*other.x - y*other.y - z*other.z;
+	newQ.x = w*other.x + x*other.w + y*other.z - z*other.y;
+	newQ.y = w*other.y + y*other.w + z*other.x - x*other.z;
+	newQ.z = w*other.z + z*other.w + x*other.y - y*other.x;
+	return newQ;
+}
